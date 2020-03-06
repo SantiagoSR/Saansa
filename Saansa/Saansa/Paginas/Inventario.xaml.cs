790a289f82b0765d1efa32e8802b9b5c11359efa
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-03-06 18:00:24 -0500

    Subiendo Proceso de Inventario

---
+++ Saansa/Saansa/Paginas/Inventario.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using FreshMvvm;
+
+namespace Saansa 
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inventario : FreshBaseContentPage
+
+    {
+        public Inventario()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ Saansa/Saansa/Paginas/PaginaArticulo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FreshMvvm;
+
+namespace Saansa.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaArticulo : FreshBaseContentPage
+    {
+        public PaginaArticulo()
+        {
+            InitializeComponent();
+        }
+    }
+}
