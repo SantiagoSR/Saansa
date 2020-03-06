4bfeb8b474903a208de08d29e82b9ff1c6d488b9
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-03-06 17:58:28 -0500

    Subiendo Proceso de articulos

--- Saansa/Saansa.Android/MainActivity.cs
+++ Saansa/Saansa.Android/MainActivity.cs
@@ -6,12 +6,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FreshMvvm;
+
 
 namespace Saansa.Droid
 {
     [Activity(Label = "Saansa", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+       
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -21,6 +24,11 @@ namespace Saansa.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+ 
+            var repository = new Repositorio(AyudaAccesoArchivo.ObtenerPathDeArchivoLocal("articulo.db3"));
+
+            FreshIOC.Container.Register(repository);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
