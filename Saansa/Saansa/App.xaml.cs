0d0e10eddef3f97f62de42e607cd7c6c4394ab65
samorenoq <samorenoq@eafit.edu.co>
2020-03-12 17:05:32 -0500

    Creado archivo generador de códigos QR

--- Saansa/Saansa/App.xaml.cs
+++ Saansa/Saansa/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using SQLite;
---
+++ Saansa/Saansa/Modelos/QRGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Saansa
+{
+    public class QRGenerator
+    {
+        ZXingBarcodeImageView barcode;
+
+        public QRGenerator(string value)
+        {
+            barcode = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                AutomationId = "zxingBarcodeImageView",
+            };
+
+            barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            barcode.BarcodeOptions.Width = 300;
+            barcode.BarcodeOptions.Height = 300;
+            barcode.BarcodeOptions.Margin = 10;
+
+            barcode.BarcodeValue = value;
+        }
+        /*
+        public Task SaveQRImage(string text)
+        {
+            var barcodeWriter = new ZXing.BarcodeWriterGeneric
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = 300,
+                    Height = 300,
+                    Margin = 10
+                }
+            };
+        }
+        */
+    }
+}
